5cae12f830ccff5382e319996a77e088b9f25c16
poper12 <samyakdhengle89@gmail.com>
2025-04-07 09:21:14 +0530

    config.py

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ env_vars = {
   # Get From my.telegram.org
   "API_ID": "20373005",
   #Get For @BotFather
-  "BOT_TOKEN": "7832627909:AAGGsogMeltJoMwacD5obZIci3sHJsIVzSw",
+  "BOT_TOKEN": "7903390136:AAFbg39h3qmR3kZD3lsZwhlRyPjTAEC4KQ4",
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "postgresql://postgres:C4VEgkZOIsrV0to9@rustically-undaunted-mastodon.data-1.use1.tembo.io:5432/postgres",
+  "DATABASE_URL_PRIMARY": "postgresql://postgres:4NQDGksWwMhyfcP5@delicately-purposeful-hen.data-1.apse1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
-  "CACHE_CHANNEL": "dump_manhwa",
+  "CACHE_CHANNEL": "Dump2075",
   # Force Subs Channel username without @
-  "CHANNEL": "Manga_Sect",
+  "CHANNEL": "",
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
-  "FNAME": "[MS] [{chap_num}] {chap_name} @Manga_Sect"  
+  "FNAME": "[MC] [{chap_num}] {chap_name} @Manga_Campus"  
 
 }
 dbname = env_vars.get('DATABASE_URL_PRIMARY') or env_vars.get('DATABASE_URL') or 'sqlite:///test.db'
