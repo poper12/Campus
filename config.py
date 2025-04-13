1fa0a7c73808d1c1079a5e8206f424481d7b74c3
Aaru <aryarai1569@protonmail.com>
2025-04-13 19:51:15 +0530

    config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ env_vars = {
   # Get From my.telegram.org
   "API_ID": "20373005",
   #Get For @BotFather
-  "BOT_TOKEN": "",
+  "BOT_TOKEN": "7903390136:AAHwXTZXqwrAir5IdLz3M8QC8Q2N_uj7fWc",
   # Get For tembo.io
   "DATABASE_URL_PRIMARY": "postgresql://postgres:4NQDGksWwMhyfcP5@delicately-purposeful-hen.data-1.apse1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
