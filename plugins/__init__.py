c7e9a1cd5ab775a1bdfcd84cae036b5632fee23d
poper12 <samyakdhengle89@gmail.com>
2025-05-24 00:56:12 +0530

    __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,7 +1,7 @@
 from .client import MangaClient, MangaCard, MangaChapter
 
 # Not Working for render/koyeb [ONLY VPS]
-#from .mgecko import MgekoClient
+from .mgecko import MgekoClient
 #from .asurascans import AsuraScansClient
 
 #Manhwa working clients
@@ -9,10 +9,10 @@ from .mangadex import MangaDexClient
 from .manhwaclan import ManhwaClanClient
 from .comick import ComickClient
 from .mangabuddy import MangaBuddyClient
-from .reaperscans import ReaperScansClient
 from .mangamob import mangamob
 from .manhuafast import ManhuaFastClient
 #from .weebcentral import WeebCentralClient
+#from .reaperscans import ReaperScansClient
 
 #18+ clients
 from .omegascans import OmegaScansClient
