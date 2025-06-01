5af132820ff3080e9c27fc24b811ddef71f2b88d
poper12 <samyakdhengle89@gmail.com>
2025-06-01 19:48:04 +0530

    config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ env_vars = {
   #Get For @BotFather
   "BOT_TOKEN": "7903390136:AAH8IL4ZajmK8MyQv8-nwEsU0xfZ3mWPLlA",
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "postgresql://postgres:ZBDQ5qhw4U9n0mil@peaceably-present-anteater.data-1.use1.tembo.io:5432/postgres",
+  "DATABASE_URL_PRIMARY": "postgresql://postgres:Testingletsee@911#@db.jxvbeathgzrzzoasuvot.supabase.co:5432/postgres",
   # Logs Channel Username Without @
   "CACHE_CHANNEL": "Dump2075",
   # Force Subs Channel username without @
